f87351a32fad806afcba4e0ad9836a7ec9360fcb
andreapirrotta <andreapirrotta1@gmail.com>
2023-03-08 13:38:15 +0100

    scheletro modificato ancora

--- DataFlowPlatform/src/coordinator.cpp
+++ DataFlowPlatform/src/coordinator.cpp
@@ -4,16 +4,19 @@ using namespace omnetpp;
 
 class Coordinator: public cSimpleModule{
     /*
-    The coordinator has an array workersData, containing informations about the id, type, task executing (index of the array GlobalData containing a subset of the input data) and state of each worker.
-    Initially the Coordinator read and validates the JSON file with the map and the reduce and the input data. Then divides the input into tasks and populates the GlobalData, Map and Reduce structure.
+
+    //Modify queue 
+
+    The coordinator has an array workersData, containing informations about the id, type, task executed (array of index of tasks done) and state of each worker.
+    Initially the Coordinator read and validates the JSON file with the map and the reduce and the input data. Then divides the input into tasks and populates the GlobalData, Map, Reduce and taskQueue structure.
     
     At Runtime:
     First the coordinator sends a defineMap and defineReduce to all workers, to tell them of the current Map and Reduce. Also sends a setData to mappers with a pointer to the GlobalData.
     The coordinator sends and executeTask message to a mapper, with the index of the GlobalData to read. Then it will update the workersData structure.
-    When a taskCompletion message is recieved the info about that worker is updated, another task from the failedTaskQueue or the GlobalData is scheduled on that free mapper with the usual executeTask message. 
+    When a taskCompletion message is recieved the info about that worker is updated, another task from the queue is scheduled on that free mapper with the usual executeTask message. 
 
     The coordinator will also periodically ping all workers by sending a ping and scheduling a self pingTimeout message, if no pong is recieved before a pingTimeout for that worker then it is considered failed and its worker data is updated. 
-    If a mapper fails its task is put into the failedTaskQueue.
+    If a mapper fails its task is put on top of the queue.
     If a reducer fails all its work is lost, we wait for it to come back online.
     
     When a back online message is recieved the info about the worker is updated. If it is a mapper a new task is scheduled, if it is a reducer all mappers are informed and send the corresponding key.
@@ -43,6 +46,6 @@ class Coordinator: public cSimpleModule{
         Map: map program;
         Reduce: reduce program;
         workersData: contains id, type, task, working or failed;
-        failedTaskQueue, failedReducerQueue: queue of int;
+        taskQueue: queue of int;
     */
 }
--- DataFlowPlatform/src/worker.cpp
+++ DataFlowPlatform/src/worker.cpp
@@ -16,9 +16,11 @@ class Worker: public cSimpleModule{
 }
 
 class Mapper{
+    //Remove terminator
+
     /*
     recieves the map from the coordinator with a defineMap message and saves the list of instructions, recieves the GlobalData structure. Recieves the executeTask message from the coordinator and fails with an x% chance. If it fails then its failed flag becomes true and set a timer for a Recovery self message, after recieveing the flag becomes false and a backonline message is sent to the coordinator. If it does not fail then the task is executed and an executionTime selfmessage is schedules, in the meanwhile every message recieved is delayed and the flag executing is set.
-    After the executionTime is recieved the computation is completed, the mapper saves the output locally, performs the shuffle and adds a terminator pair, then computes using mod the reducers it will have to send keys to and send them a getPairs. The mapper also sends a mapCompletion to the coordinator.
+    After the executionTime is recieved the computation is completed, the mapper saves the output locally, performs the shuffle, then computes using mod the reducers it will have to send keys to and send them a getPairs. The mapper also sends a mapCompletion to the coordinator.
     if a resendKey message is recieved the pairs for a reducer are sent again.
 
     Messages:
@@ -46,7 +48,7 @@ class Mapper{
 class Reducer{
     /*
     recieves from the coordinator a setReduce message.
-    recieves from the mapper the getPairs message and starts retrieving the pairs that belong to it, after the terminator pais is read and saved the index of the task is set as fully read in tasksRead.
+    recieves from the mapper the getPairs message and starts retrieving the pairs that belong to it, after that the task is set as fully read in tasksRead.
     the reducer initiates the reduce task and randomly fails. If it does not fail then it schedules an executionTime selfmessage and performs the reduce, in the meanwhile the executing flag is set. If it fails then its failed flag becomes true and set a timer for a Recovery self message, after recieveing the flag becomes false and a backonline message is sent to the coordinator.
     if a taskDone is recieved the output is written and the computation ends.
     if a getPairs is recieved for a task already read the reducer skips them.
